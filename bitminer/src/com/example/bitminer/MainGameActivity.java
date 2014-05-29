3dc6ee94b7797d356012558e8d6008a290328eff
Scott <Scott@192.168.11.103>
2014-05-29 16:01:44 -0400

    animation update for touch

--- bitminer/src/com/example/bitminer/MainGameActivity.java
+++ bitminer/src/com/example/bitminer/MainGameActivity.java
@@ -8,6 +8,7 @@ import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
 import org.andengine.entity.sprite.AnimatedSprite;
 import org.andengine.entity.sprite.Sprite;
+import org.andengine.input.touch.TouchEvent;
 import org.andengine.opengl.texture.TextureOptions;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
@@ -15,6 +16,8 @@ import org.andengine.opengl.texture.region.TextureRegion;
 import org.andengine.opengl.texture.region.TiledTextureRegion;
 import org.andengine.ui.activity.SimpleBaseGameActivity;
 import org.andengine.util.color.Color;
+
+import android.view.MotionEvent;
 
 public class MainGameActivity extends SimpleBaseGameActivity {
 	
@@ -55,10 +58,17 @@ public class MainGameActivity extends SimpleBaseGameActivity {
 	  m_Scene = new Scene();
 	  m_Scene.setBackground(new Background(Color.WHITE));
 	 
-	  sprMiner = new AnimatedSprite(150, 100, regMiner, this.getVertexBufferObjectManager());
+	  sprMiner = new AnimatedSprite(150, 100, regMiner, this.getVertexBufferObjectManager())	{
+		  @Override
+		  public boolean onAreaTouched(TouchEvent pSceneTouchEvent, float pTouchAreaLocalX, float pTouchAreaLocalY)	{
+			  if(pSceneTouchEvent.getAction() == TouchEvent.ACTION_DOWN)	{
+				  sprMiner.animate(new long[] {150, 150, 150, 150, 200}, 0, 4, 3);
+			  }
+			  return true;
+		  }
+	  };
+	  m_Scene.registerTouchArea(sprMiner);
 	  m_Scene.attachChild(sprMiner);
-	 
-	  sprMiner.animate(150);
 	 
 	  return m_Scene;
 	 }
